4ce00b94595001e7f3ca3434cbcba5b8bca846aa
C_A_R_L_O_S <vargasarcayacarlos@gmail.com>
2024-09-23 11:11:10 -0400

    Practica completa mejorada

--- Practica-CodigosPython/Ej8.py
+++ Practica-CodigosPython/Ej8.py
@@ -1,5 +1,6 @@
 #Hallar el factorial de todos os numeros comprendidios en el rango (a...b)
 #Donde a debe ser menor que b
+#Para correr correctamente el programa a debe ser manor que b
 a=int(input("Introduce el valir de a: "))
 b=int(input("Introduce el valor de b: "))
 fac=1; i=1
--- Practica-CodigosPython/Ej8MejorVersion.py
+++ Practica-CodigosPython/Ej8MejorVersion.py
@@ -1,3 +1,7 @@
+#Hallar el factorial de todos os numeros comprendidios en el rango (a...b)
+#Donde a debe ser menor que b
+#Este esta hecho con el contador automatico, en lo general imprime mas limpiamente el i
+#Para correr correctamente el programa a debe ser manor que b
 a=int(input("Intro a: "))
 b=int(input("Intro b: "))
 for i in range(a,b+1):
