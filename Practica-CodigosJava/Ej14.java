8fb9f27b29a018d16b4c5648e49c2f68028925df
C_A_R_L_O_S <vargasarcayacarlos@gmail.com>
2024-09-23 10:42:10 -0400

    Practica Completa Corregida

--- Practica-CodigosJava/Ej14.java
+++ Practica-CodigosJava/Ej14.java
@@ -1,2 +1,24 @@
 //Dados 3 números: a, b y c, mostrar los múltiplos de a en el rango de b y c. 
 //Ejmemplo: a=3,b=5,c=11 → 6,9
+import java.util.Scanner;
+public class Ej14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce a: ");
+        int a = scanner.nextInt();
+        System.out.print("Introduce b: ");
+        int b = scanner.nextInt();
+        System.out.print("Introduce c: ");
+        int c = scanner.nextInt();
+        int v = (b / a) + 1;
+        int ma = a * v;
+        while (ma <= c) {
+            if (ma > b) {
+                System.out.print(ma + ", ");
+            }
+            v++;
+            ma = a * v;
+        }
+        scanner.close();
+    }
+}
